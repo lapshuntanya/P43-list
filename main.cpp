a6334724fc8d6e42de38c2d7e67be2b2a035f0ff
Tetiana Perederii <lapshun.tatiana@gmail.com>
2025-11-19 17:29:11 +0200

    main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include "MyList.h"
 
+int main() {
+    //7 3 9 2
+    MyList::Node<int> first(7); //info = 7, next = null
+
+    MyList::Node<int> second(3);// info = 3, next = null
+    first.next = &second;
+
+    MyList::Node<int> third(9); //info = 9, next = null
+    second.next = &third;
+
+    MyList::Node<int> fourth(2);//info = 2, next = null
+    third.next = &fourth;
+
+
+
+
+    return 0;
+}
