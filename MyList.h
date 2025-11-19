c26154bbf660ba55d6956f2bcca996ad3159d321
Tetiana Perederii <lapshun.tatiana@gmail.com>
2025-11-19 18:21:15 +0200

    void push_back(U value)

--- MyList.h
+++ MyList.h
@@ -27,6 +27,36 @@ namespace MyList {
             << "\t NEXT: " << next << endl;
         }
     };
+
+    template <typename  U>
+    class List {
+        DNode<U>* head;
+        DNode<U>* tail;
+    public:
+        List() {
+            head = tail = nullptr;
+        }
+        ~List() {
+
+        }
+
+        void push_back(U value) {
+            //1 - Створюємо вузол
+            DNode<U>* el = new DNode<U>(value);
+            //2 Чи створили перший вузол
+            if (head == nullptr) {
+                head = el;
+                tail = el;
+            }else {// 2, 3, ... інші вузли
+                //Звʼязуємо вузли
+                tail->next = el;
+                el->prev = tail;
+                //Оновлюємо хвіст
+                tail = el;
+            }
+        }
+
+    };
 }
 
 
